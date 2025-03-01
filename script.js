4f92d56fe6acfae878e4241fcfb4c7044f03927a
LOSEARDES77 <loseardes77@gmail.com>
2025-03-01 20:20:32 +0100

    Arreglar rotacion de barcos en otros navegadores, añadir overlay de informacion

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 class Jugador {
     constructor() {
-        this.element = document.getElementById('turno');
+        this.element = document.getElementById("turno");
         this.turno = true; // True: Rojo, False: Amarillo
         this.actualizarTurno();
     }
 
     actualizarTurno() {
-        this.element.innerHTML = `Turno de <span style='color: ${this.turno ? '#ff0000' : '#ffff00'};'>${this.turno ? 'Barba Roja' : 'Pata de Palo'}</span>`;
+        this.element.innerHTML = `Turno de <span style='color: ${
+            this.turno ? "#ff0000" : "#ffff00"
+        };'>${this.turno ? "Barba Roja" : "Pata de Palo"}</span>`;
     }
 
     cambiarTurno() {
@@ -23,38 +25,41 @@ class Tablero {
         this.barcosColocados = 0;
         this.jugador = jugador;
         this.clicks = 0;
+        this.casillas_tocadas = 0;
+        this.barcos_hundidos = 0;
+        this.fallos = 0;
     }
 
     crearTablero() {
-        let html = '<table>';
+        let html = "<table>";
         for (let i = 0; i < this.tamanio; i++) {
-            html += '<tr>';
+            html += "<tr>";
             for (let j = 0; j < this.tamanio; j++) {
                 html += `<td id="c${i}${j}"></td>`;
             }
-            html += '</tr>';
+            html += "</tr>";
         }
-        html += '</table>';
+        html += "</table>";
         this.tablero.innerHTML = html;
-        const celdas = document.querySelectorAll('td');
-        celdas.forEach(celda => {
-            celda.addEventListener('click', (e) => this.handleClick(e));
-            celda.addEventListener('dragover', (e) => this.dragOver(e));
-            celda.addEventListener('drop', (e) => this.drop(e));
+        const celdas = document.querySelectorAll("td");
+        celdas.forEach((celda) => {
+            celda.addEventListener("click", (e) => this.handleClick(e));
+            celda.addEventListener("dragover", (e) => this.dragOver(e));
+            celda.addEventListener("drop", (e) => this.drop(e));
         });
     }
 
     dragOver(e) {
         e.preventDefault();
-
     }
 
     drop(e) {
         e.preventDefault();
-        if (this.jugador.turno !== 'Rojo') return;
-        const barco = document.querySelector('.dragging');
-        const size = parseInt(barco.getAttribute('data-size'));
-        const orientation = barco.getAttribute('data-orientation') || 'horizontal';
+        if (!this.jugador.turno) return;
+        const barco = document.querySelector(".dragging");
+        const size = parseInt(barco.getAttribute("data-size"));
+        const orientation =
+            barco.getAttribute("data-orientation") || "horizontal";
         const celda = e.target;
         const row = parseInt(celda.id[1]);
         const col = parseInt(celda.id[2]);
@@ -62,10 +67,14 @@ class Tablero {
         // Check if the ship can be placed
         if (this.canPlaceShip(row, col, size, orientation)) {
             for (let i = 0; i < size; i++) {
-                if (orientation === 'horizontal') {
-                    document.getElementById(`c${row}${col + i}`).classList.add('ship');
+                if (orientation === "horizontal") {
+                    document
+                        .getElementById(`c${row}${col + i}`)
+                        .classList.add("ship");
                 } else {
-                    document.getElementById(`c${row + i}${col}`).classList.add('ship');
+                    document
+                        .getElementById(`c${row + i}${col}`)
+                        .classList.add("ship");
                 }
             }
             barco.remove();
@@ -74,16 +83,20 @@ class Tablero {
             if (this.barcosColocados === 6) {
                 this.ocultarBarcos();
                 this.jugador.cambiarTurno();
+                this.actualizarInfo();
             }
         } else {
-            alert('No se puede colocar el barco aquí.');
+            alert("No se puede colocar el barco aquí.");
         }
     }
 
     canPlaceShip(row, col, size, orientation) {
         for (let i = 0; i < size; i++) {
-            const cell = orientation === 'horizontal' ? document.getElementById(`c${row}${col + i}`) : document.getElementById(`c${row + i}${col}`);
-            if (cell === null || cell.classList.contains('ship')) {
+            const cell =
+                orientation === "horizontal"
+                    ? document.getElementById(`c${row}${col + i}`)
+                    : document.getElementById(`c${row + i}${col}`);
+            if (cell === null || cell.classList.contains("ship")) {
                 return false;
             }
         }
@@ -91,103 +104,147 @@ class Tablero {
     }
 
     ocultarBarcos() {
-        const celdas = document.querySelectorAll('.ship');
-        celdas.forEach(celda => {
-            celda.classList.remove('ship');
-            celda.classList.add('hidden-ship');
+        const celdas = document.querySelectorAll(".ship");
+        celdas.forEach((celda) => {
+            celda.classList.remove("ship");
+            celda.classList.add("hidden-ship");
         });
     }
 
     handleClick(e) {
         const celda = e.target;
-        document.getElementById('clicks').innerText = `LLevas ${this.clicks} disparos.`;
-        if (this.jugador.turno === 'Amarillo') {
-            if (celda.classList.contains('hidden-ship')) {
-                if (celda.classList.contains('ship-hit') || celda.classList.contains('ship-miss')) return;
-                this.clicks++;
-                celda.classList.add('ship-hit');
-                celda.innerHTML = 'X';
-                this.markHit(celda);
-                this.checkAllShipsSunk();
-            } else {
-                celda.classList.add('ship-miss');
-                celda.innerHTML = 'A';
-                mostrarMensaje('💧 Agua 💧');
-            }
+        if (this.jugador.turno) return;
+        if (celda.classList.contains("ship-hit")) return;
+        if (celda.classList.contains("ship-miss")) return;
+        if (celda.classList.contains("hidden-ship")) {
+            this.clicks++;
+            document.getElementById(
+                "clicks"
+            ).innerText = `Llevas ${this.clicks} disparos.`;
+            celda.classList.add("ship-hit");
+            celda.innerHTML = "X";
+            this.markHit(celda);
+            this.checkAllShipsSunk();
+        } else {
+            celda.classList.add("ship-miss");
+            this.clicks++;
+            this.fallos++;
+            celda.innerHTML = "A";
+            mostrarMensaje("💧 Agua 💧");
         }
+        this.actualizarInfo();
     }
 
     markHit(celda) {
         const row = parseInt(celda.id[1]);
         const col = parseInt(celda.id[2]);
 
-        const ship = this.ships.find(ship => {
-            if (ship.orientation === 'horizontal') {
-                return ship.row === row && col >= ship.col && col < ship.col + ship.size;
+        const ship = this.ships.find((ship) => {
+            if (ship.orientation === "horizontal") {
+                return (
+                    ship.row === row &&
+                    col >= ship.col &&
+                    col < ship.col + ship.size
+                );
             } else {
-                return ship.col === col && row >= ship.row && row < ship.row + ship.size;
+                return (
+                    ship.col === col &&
+                    row >= ship.row &&
+                    row < ship.row + ship.size
+                );
             }
         });
 
         if (ship) {
             ship.hits++;
-            mostrarMensaje('🩸 TOCADO 🩸');
+            this.casillas_tocadas++;
+            mostrarMensaje("🩸 Tocado 🩸");
             if (ship.hits === ship.size) {
                 this.markSunkShip(ship);
-                mostrarMensaje('💣 TOCADO Y HUNDIDO. HURRA! 💣');
+                this.barcos_hundidos++;
+                mostrarMensaje("💣 Tocado y hundido. ¡Hurra! 💣");
             }
         }
     }
 
     markSunkShip(ship) {
         for (let i = 0; i < ship.size; i++) {
-            const cell = ship.orientation === 'horizontal' ? document.getElementById(`c${ship.row}${ship.col + i}`) : document.getElementById(`c${ship.row + i}${ship.col}`);
+            const cell =
+                ship.orientation === "horizontal"
+                    ? document.getElementById(`c${ship.row}${ship.col + i}`)
+                    : document.getElementById(`c${ship.row + i}${ship.col}`);
             if (cell) {
-                cell.classList.add('ship-sunk');
+                cell.classList.add("ship-sunk");
             }
         }
     }
 
     checkAllShipsSunk() {
-        const allSunk = this.ships.every(ship => ship.hits === ship.size);
+        const allSunk = this.ships.every((ship) => ship.hits === ship.size);
         if (allSunk) {
+            // Timeout para que siga actualizando el tablero en pantalla
             setTimeout(() => {
-                alert('¡Todos los barcos han sido hundidos!');
+                let msg = "¡Todos los barcos han sido hundidos!\n\n";
+                msg += "Disparos: " + this.clicks + "\n";
+                msg += "Barcos hundidos: " + this.barcos_hundidos + "\n";
+                msg += "Fallos: " + this.fallos + "\n";
+                msg +=
+                    "Puntería: " +
+                    ((this.casillas_tocadas / this.clicks) * 100).toFixed(2) +
+                    "%\n";
+                msg += "¡Gracias por jugar! ¡Hasta la próxima!\n";
+                alert(msg);
                 window.location.reload();
-            }, 500);
+            }, 200);
         }
+    }
+
+    actualizarInfo() {
+        document.getElementById(
+            "clicks"
+        ).innerText = `Llevas ${this.clicks} disparos.`;
+        document.getElementById("info-batalla").innerText =
+            `Barcos tocados: ${this.casillas_tocadas}` +
+            ` | Barcos hundidos: ${this.barcos_hundidos}/6` +
+            ` | Fallos: ${this.fallos}` +
+            ` | Puntería: ${(
+                (this.casillas_tocadas / this.clicks) *
+                100
+            ).toFixed(2)}%`;
     }
 }
 
 function mostrarMensaje(mensaje) {
     const divMensaje = document.getElementById("mensaje");
-    if(divMensaje) {
-      divMensaje.innerText = mensaje;
+    if (divMensaje) {
+        divMensaje.innerText = mensaje;
     }
-  }
+}
 
 window.onload = () => {
     const jugador = new Jugador();
-    const tablero = new Tablero('tablero', 10, jugador);
+    const tablero = new Tablero("tablero", 10, jugador);
+    const barcos = document.querySelectorAll(".barco");
+
     tablero.crearTablero();
 
-    const barcos = document.querySelectorAll('.barco');
-    document.addEventListener('keydown', (e) => {
-        if (e.key === 'r' || e.key === 'R') {
-            const barco = document.querySelector('.dragging');
-            if (!barco) return;
-            const currentOrientation = barco.getAttribute('data-orientation') || 'horizontal';
-            const newOrientation = currentOrientation === 'horizontal' ? 'vertical' : 'horizontal';
-            barco.setAttribute('data-orientation', newOrientation);
-            barco.classList.toggle('vertical');
-        }
+    document.addEventListener("dblclick", (e) => {
+        const barco = e.target.closest(".barco");
+        if (!barco) return;
+        const currentOrientation =
+            barco.getAttribute("data-orientation") || "horizontal";
+        const newOrientation =
+            currentOrientation === "horizontal" ? "vertical" : "horizontal";
+        barco.setAttribute("data-orientation", newOrientation);
+        barco.classList.toggle("vertical");
     });
-    barcos.forEach(barco => {
-        barco.addEventListener('dragstart', () => {
-            barco.classList.add('dragging');
+
+    barcos.forEach((barco) => {
+        barco.addEventListener("dragstart", () => {
+            barco.classList.add("dragging");
         });
-        barco.addEventListener('dragend', () => {
-            barco.classList.remove('dragging');
+        barco.addEventListener("dragend", () => {
+            barco.classList.remove("dragging");
         });
     });
-}
+};
